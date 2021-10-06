12ac3a90c92e5ff966eae8dd50fcc04058683842
Sam Warner <sam.warner@bravissimo.com>
2021-10-06 11:59:18 +0100

    Add alt text to images

--- src/SearchResultItem.jsx
+++ src/SearchResultItem.jsx
@@ -4,7 +4,7 @@ const SearchResultItem = ({item}) => {
     return (
         <div className="searchResult">
         <p>{item.styleName}</p>
-        <img src={item.colours[0].images.detail.src}></img>
+        <img src={item.colours[0].images.detail.src} alt={item.colours[0].images.detail.alt}></img>
         </div>
     );
 }
