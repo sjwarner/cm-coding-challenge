122e0b7861b71ae14009c91bb05692ccaa25e313
Sam Warner <sam.warner@bravissimo.com>
2021-10-06 11:42:09 +0100

    Render prices

--- src/SearchResultItem.jsx
+++ src/SearchResultItem.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchResultItem = ({item}) => {
     return (
         <div className="searchResult">
+        <p>£{item.colours[0].pricing.originalPrice}</p>
         <p>{item.styleName}</p>
         <img src={item.colours[0].images.detail.src}></img>
         </div>
